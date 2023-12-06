d4afd00fa7ec965a2681284dfd7fd0c2959a9969
alyona.chekurova <alyona.chekurova@simbirsoft.com>
2023-12-06 11:14:37 +0500

    Рефакторинг названий, удален Waiters

--- src/test/java/tests/NotesScreenTests.java
+++ src/test/java/tests/NotesScreenTests.java
@@ -7,9 +7,9 @@ import java.io.IOException;
 /**
  * Класс с тестами приложения
  */
-public class MobileAppTests extends BaseTest{
+public class NotesScreenTests extends BaseTest{
 
-    @Test
+    @Test(description = "Mob-1: Установка и открытие приложения")
     public void openAppTest() throws IOException {
         setUp();
         openMainNotesPage()
